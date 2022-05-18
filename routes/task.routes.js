7c8e1b0ea5108fc907dce0395c1ecee739cbadd4
jidekickpush <jeandenisdrane@gmail.com>
2022-05-18 10:20:14 +0200

    Add all routes for Tasks functionality

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -24,4 +24,68 @@ router.post('/tasks', (req, res, next) => {
         .catch(err => res.json(err));
 });
 
+// Retrieves all of the task
+router.get('/tasks', (req, res, next) => {
+    Task.find()
+      .populate('project')
+      .then(allTasks => res.status(200).json(allTasks))
+      .catch(err => {
+        console.log("error getting all tasks from DB",err)
+        res.status(500).json({
+          message: "error getting all tasks from DB",
+          error: err}
+          )});
+  });
+
+// Retrieves a specific project by id
+router.get('/tasks/:tasksId', (req, res, next) => {
+    const { tasksId } = req.params;
+   
+    if (!mongoose.Types.ObjectId.isValid(tasksId)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+   
+    // Each Project document has `tasks` array holding `_id`s of Task documents
+    // We use .populate() method to get swap the `_id`s for the actual Task documents
+    Task.findById(tasksId)
+      .populate('project')
+      .then(task => res.status(200).json(task))
+      .catch(err => {
+        console.log("error getting a specific tasks from DB",err)
+        res.status(500).json({
+          message: "error getting a specific tasks from DB",
+          error: err}
+          )});
+  });
+
+
+// Updates a specific task by id
+router.put('/tasks/:taskId', (req, res, next) => {
+    const { taskId } = req.params;
+   
+    if (!mongoose.Types.ObjectId.isValid(taskId)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+   
+    Task.findByIdAndUpdate(taskId, req.body, { new: true })
+      .then((updatedtask) => res.json(updatedtask))
+      .catch(error => res.json(error));
+  });
+
+  // Deletes a specific task by id
+router.delete('/tasks/:tasksId', (req, res, next) => {
+    const { tasksId } = req.params;
+    
+    if (!mongoose.Types.ObjectId.isValid(tasksId)) {
+      res.status(400).json({ message: 'Specified id is not valid' });
+      return;
+    }
+   
+    Task.findByIdAndRemove(tasksId)
+      .then(() => res.json({ message: `ttasks with ${tasksId} is removed successfully.` }))
+      .catch(error => res.json(error));
+  });
+
 module.exports = router;
